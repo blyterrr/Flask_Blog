bb079858ac3c9d02f6a566da5099c9d96bf598c5
andrei <blyterrr@protonmail.com>
2020-01-21 14:23:06 -0500

    Did Part 3 of Corey Schafer's Flask tutorial
    - added login and registration functions, with validation

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, escape, request, render_template, url_for
+from flask import Flask, escape, request, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '1478228fe97868c31d03c5aeebc807d2'
 
 posts = [
     {
@@ -25,6 +28,28 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title = 'About')
+
+@app.route('/registration', methods = ['GET', 'POST'])
+def registration():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for: {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('registration.html', title = 'Register', form = form)
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == 'admin' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccesful. Please check credentials', 'danger')
+
+    return render_template('login.html', title = 'Login', form = form)
+
 
 if __name__ == '__main__':
     app.run(debug = True)
